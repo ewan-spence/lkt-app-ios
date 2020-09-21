3918e9e0233261c446f0c005efd4927d27517025
ewan-spence <ewan_spence@btinternet.com>
2020-09-21 17:43:12 +0100

    Boilerplate app with successful client login using API

---
+++ Let's Keep Talking/CallerLoginView.swift
@@ -0,0 +1,20 @@
+//
+//  CallerLoginView.swift
+//  Let's Keep Talking
+//
+//  Created by Ewan Spence on 19/09/2020.
+//
+
+import SwiftUI
+
+struct CallerLoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CallerLoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        CallerLoginView()
+    }
+}
