d23a8c44d5d4f80719a75bc7bd57df62c367736e
ewan-spence <ewan_spence@btinternet.com>
2020-11-07 16:57:03 +0000

    Adds two tabs to Client and Caller call logs

--- Let's Keep Talking/CallerCallLogView.swift
+++ Let's Keep Talking/CallerCallLogView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct CallerCallLogView: View {
+    @State var isOnAllCalls: Bool = true
+    @State var isOnFutureCalls: Bool = false
     
     @Binding var calls: [[String: String]]?
     
@@ -19,11 +21,60 @@ struct CallerCallLogView: View {
             
             Spacer()
             
+            HStack {
+                ZStack {
+                    HStack {
+                        Spacer()
+                        
+                        Text("All Calls")
+                            .onTapGesture(perform: {
+                                isOnAllCalls = true
+                                isOnFutureCalls = false
+                            })
+                        
+                        Spacer()
+                    }
+                    
+                    if(isOnAllCalls) {
+                        RoundedRectangle(cornerRadius: 15)
+                            .stroke(Color.green, lineWidth: 5)
+                    }
+                }.frame(height: 40, alignment: .center)
+                
+                ZStack {
+                    HStack {
+                        Spacer()
+                        
+                        Text("Future Calls")
+                            .onTapGesture(perform: {
+                                isOnAllCalls = false
+                                isOnFutureCalls = true
+                            })
+                        
+                        Spacer()
+                    }
+                    if(isOnFutureCalls) {
+                        RoundedRectangle(cornerRadius: 15)
+                            .stroke(Color.green, lineWidth: 5)
+                    }
+                }.frame(height: 40, alignment: .center)
+            }
+                        
             ScrollView {
-                ForEach((calls ?? []).sorted(by: Helpers.sortCalls), id: \.self) { call in
-                    AppointmentRowView(call: call, isClient: false, isOnCallLog: true)
+                if(isOnAllCalls) {
+                    ForEach((calls ?? []).sorted(by: Helpers.sortCalls), id: \.self) { call in
+                        AppointmentRowView(call: call, isClient: false, isOnCallLog: true)
+                    }
+                }
+                if(isOnFutureCalls) {
+                    ForEach((calls ?? []).filter({ call in
+                        Helpers.isInFuture(call["date"]!, call["time"]!)
+                    }).sorted(by: Helpers.sortCalls).reversed(), id: \.self) { call in
+                        AppointmentRowView(call: call, isClient: false, isOnCallLog: true)
+                    }
                 }
             }
+            
         }
         
     }
--- Let's Keep Talking/ClientCallLogView.swift
+++ Let's Keep Talking/ClientCallLogView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ClientCallLogView: View {
+    @State var isOnAllCalls: Bool = true
+    @State var isOnFutureCalls: Bool = false
     
     // Assume call is a dictionary of strings with keys: date, time, callerName, id
     @Binding var calls: [[String: String]]?
@@ -20,10 +22,61 @@ struct ClientCallLogView: View {
                     .padding()
                 
                 Spacer()
+                
+                HStack {
+                    ZStack {
+                        HStack {
+                            Spacer()
+                            
+                            Text("All Calls")
+                                .onTapGesture(perform: {
+                                    isOnAllCalls = true
+                                    isOnFutureCalls = false
+                                })
+                            
+                            Spacer()
+                        }
+                        
+                        if(isOnAllCalls) {
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.green, lineWidth: 5)
+                        }
+                    }.frame(height: 40, alignment: .center)
+                    
+                    ZStack {
+                        HStack {
+                            Spacer()
+                            
+                            Text("Future Calls")
+                                .onTapGesture(perform: {
+                                    isOnAllCalls = false
+                                    isOnFutureCalls = true
+                                })
+                            
+                            Spacer()
+                        }
+                        if(isOnFutureCalls) {
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.green, lineWidth: 5)
+                        }
+                    }.frame(height: 40, alignment: .center)
+                }
             
                 ScrollView {
-                    ForEach((calls ?? []).sorted(by: Helpers.sortCalls), id: \.self) { call in
-                        AppointmentRowView(call: call, isClient: true, isOnCallLog: true)
+                    
+                    if(isOnAllCalls) {
+                        ForEach((calls ?? []).sorted(by: Helpers.sortCalls), id: \.self) { call in
+                            AppointmentRowView(call: call, isClient: true, isOnCallLog: true)
+                        }
+                    }
+
+                    if(isOnFutureCalls) {
+                        ForEach((calls ?? []).filter({call in
+                            Helpers.isInFuture(call["date"]!, call["time"]!)
+                        }).sorted(by: Helpers.sortCalls), id: \.self) { call in
+                            AppointmentRowView(call: call, isClient: true, isOnCallLog: true)
+
+                        }
                     }
                 }.padding()
                 
